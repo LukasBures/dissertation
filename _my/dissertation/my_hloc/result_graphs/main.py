8257355c5797770e4558214dc1705c0c0437a819
Lukas Bures <burylukas@seznam.cz>
2021-12-12 15:03:14 +0100

    added result graphs main.py

---
+++ _my/dissertation/my_hloc/result_graphs/main.py
@@ -0,0 +1,47 @@
+
+
+aachen_test = [
+    {
+        "static_percentage": 50,
+        "dynamic_percentage": 100,
+        "day": [89.7, 95.8, 98.7],
+        "night": [87.8, 94.9, 100.0],
+    },
+    {
+        "static_percentage": 60,
+        "dynamic_percentage": 100,
+        "day": [89.7, 95.8, 98.7],
+        "night": [87.8, 94.9, 100.0],
+    },
+    {
+        "static_percentage": 70,
+        "dynamic_percentage": 100,
+        "day": [89.7, 95.8, 98.7],
+        "night": [87.8, 94.9, 100.0],
+    },
+    {
+        "static_percentage": 80,
+        "dynamic_percentage": 100,
+        "day": [89.7, 95.8, 98.7],
+        "night": [87.8, 94.9, 100.0],
+    },
+    {
+        "static_percentage": 90,
+        "dynamic_percentage": 100,
+        "day": [89.7, 95.8, 98.7],
+        "night": [87.8, 94.9, 100.0],
+    },
+    {
+        "static_percentage": 100,
+        "dynamic_percentage": 100,
+        "day": [89.7, 95.8, 98.7],
+        "night": [87.8, 94.9, 100.0],
+    },
+]
+
+aachen = [aachen_test]
+
+for a in aachen:
+    print(a)
+
+print("DONE")
