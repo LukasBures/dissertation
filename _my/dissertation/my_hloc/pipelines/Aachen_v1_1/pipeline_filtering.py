aca5134533b3ba40cdf38a14ed8f735a616aae03
Lukas Bures <burylukas@seznam.cz>
2022-01-19 22:50:34 +0100

    update name

--- _my/dissertation/my_hloc/pipelines/Aachen_v1_1/pipeline_filtering.py
+++ _my/dissertation/my_hloc/pipelines/Aachen_v1_1/pipeline_filtering.py
@@ -149,7 +149,7 @@ for static_percentage in static_percentages:
         ff = FeatureFilter(
             h5_file_path=str(all_features_pth),
             new_h5_file_path=str(new_features_pth),
-            segmentations_file=str(segmentations_file_path),
+            segmentation_h5_file_path=str(segmentations_file_path),
         )
         ff.filter_and_update_kp(static_percentage_keep=static_percentage, dynamic_percentage_keep=dynamic_percentage)
         del ff
