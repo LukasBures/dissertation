d3b7b76eb5f446407dd41a9abd18e8f4f7616032
Lukas Bures <burylukas@seznam.cz>
2022-01-30 14:56:39 +0100

    4Seasons update

--- _my/dissertation/my_hloc/pipelines/4Seasons/pipeline_filtering.py
+++ _my/dissertation/my_hloc/pipelines/4Seasons/pipeline_filtering.py
@@ -9,9 +9,14 @@ from pprint import pformat
 import pycolmap
 import torch
 from configs import feature_configs, matcher_configs
-from utils2 import get_timestamps, delete_unused_images
-from utils2 import generate_query_lists, generate_localization_pairs
-from utils2 import prepare_submission, evaluate_submission
+from utils2 import (
+    delete_unused_images,
+    evaluate_submission,
+    generate_localization_pairs,
+    generate_query_lists,
+    get_timestamps,
+    prepare_submission,
+)
 
 sys.path.append("/home/lukas/PycharmProjects/dissertation/Hierarchical-Localization")
 try:
@@ -22,23 +27,29 @@ else:
     print(f"__PYTHONPATH: {user_paths}")
 
 from feature_filter import FeatureFilter
-from hloc import extract_features, match_features, localize_sfm, logger
+from hloc import extract_features, localize_sfm, logger, match_features
 
 relocalization_files = {
-    'training': 'RelocalizationFilesTrain//relocalizationFile_recording_2020-03-24_17-36-22.txt',
-    'validation': 'RelocalizationFilesVal/relocalizationFile_recording_2020-03-03_12-03-23.txt',
-    'test0': 'RelocalizationFilesTest/relocalizationFile_recording_2020-03-24_17-45-31_*.txt',
-    'test1': 'RelocalizationFilesTest/relocalizationFile_recording_2020-04-23_19-37-00_*.txt',
+    "training": "RelocalizationFilesTrain//relocalizationFile_recording_2020-03-24_17-36-22.txt",
+    "validation": "RelocalizationFilesVal/relocalizationFile_recording_2020-03-03_12-03-23.txt",
+    "test0": "RelocalizationFilesTest/relocalizationFile_recording_2020-03-24_17-45-31_*.txt",
+    "test1": "RelocalizationFilesTest/relocalizationFile_recording_2020-04-23_19-37-00_*.txt",
 }
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--sequence', type=str, required=True,
-                    choices=['training', 'validation', 'test0', 'test1'],
-                    help='Sequence to be relocalized.')
-parser.add_argument('--dataset', type=Path, default='datasets/4Seasons',
-                    help='Path to the dataset, default: %(default)s')
-parser.add_argument('--outputs', type=Path, default='outputs/4Seasons',
-                    help='Path to the output directory, default: %(default)s')
+parser.add_argument(
+    "--sequence",
+    type=str,
+    required=True,
+    choices=["training", "validation", "test0", "test1"],
+    help="Sequence to be relocalized.",
+)
+parser.add_argument(
+    "--dataset", type=Path, default="datasets/4Seasons", help="Path to the dataset, default: %(default)s"
+)
+parser.add_argument(
+    "--outputs", type=Path, default="outputs/4Seasons", help="Path to the output directory, default: %(default)s"
+)
 parser.add_argument("--num_ref", type=int, default=20, help="Number of image pairs for SfM, default: %(default)s")
 parser.add_argument("--num_loc", type=int, default=10, help="Number of image pairs for loc, default: %(default)s")
 parser.add_argument("--feature_conf", type=str, help="Feature config")
@@ -131,52 +142,55 @@ for static_percentage in static_percentages:
         print("-" * 50)
         print(f"Starting: static: {static_percentage}%, dynamic: {dynamic_percentage}")
 
+        data_dir = args.dataset
+        ref_dir = data_dir / "reference"
+        assert ref_dir.exists(), f"{ref_dir} does not exist"
+        seq_dir = data_dir / sequence
+        assert seq_dir.exists(), f"{seq_dir} does not exist"
+        seq_images = seq_dir / "undistorted_images"
+        reloc = ref_dir / relocalization_files[sequence]
 
-data_dir = args.dataset
-ref_dir = data_dir / 'reference'
-assert ref_dir.exists(), f'{ref_dir} does not exist'
-seq_dir = data_dir / sequence
-assert seq_dir.exists(), f'{seq_dir} does not exist'
-seq_images = seq_dir / 'undistorted_images'
-reloc = ref_dir / relocalization_files[sequence]
+        output_dir = args.outputs
+        output_dir.mkdir(exist_ok=True, parents=True)
+        query_list = output_dir / f"{sequence}_queries_with_intrinsics.txt"
+        ref_pairs = output_dir / "pairs-db-dist20.txt"
+        ref_sfm = output_dir / "sfm_superpoint+superglue"
+        results_path = output_dir / f"localization_{sequence}_hloc+superglue.txt"
+        submission_dir = output_dir / "submission_hloc+superglue"
+        loc_pairs = output_dir / f"pairs-query-{sequence}-dist{num_loc_pairs}.txt"
 
-output_dir = args.outputs
-output_dir.mkdir(exist_ok=True, parents=True)
-query_list = output_dir / f'{sequence}_queries_with_intrinsics.txt'
-ref_pairs = output_dir / 'pairs-db-dist20.txt'
-ref_sfm = output_dir / 'sfm_superpoint+superglue'
-results_path = output_dir / f'localization_{sequence}_hloc+superglue.txt'
-submission_dir = output_dir / 'submission_hloc+superglue'
+        # Not all query images that are used for the evaluation.
+        # To save time in feature extraction, we delete unused images.
+        timestamps = get_timestamps(files=reloc, idx=1)
+        delete_unused_images(root=seq_images, timestamps=timestamps)
 
-num_loc_pairs = 10
-loc_pairs = output_dir / f'pairs-query-{sequence}-dist{num_loc_pairs}.txt'
+        # Generate a list of query images with their intrinsics.
+        generate_query_lists(timestamps=timestamps, seq_dir=seq_dir, out_path=query_list)
 
-fconf = extract_features.confs['superpoint_max']
-mconf = match_features.confs['superglue']
+        # Generate the localization pairs from the given reference frames.
+        generate_localization_pairs(
+            sequence=sequence, reloc=reloc, num=num_loc_pairs, ref_pairs=ref_pairs, out_path=loc_pairs
+        )
 
-# Not all query images that are used for the evaluation.
-# To save time in feature extraction, we delete unused images.
-timestamps = get_timestamps(reloc, 1)
-delete_unused_images(seq_images, timestamps)
+        # Extract, match, and localize.
+        feature_file = extract_features.main(conf=feature_conf, image_dir=seq_images, export_dir=output_dir)
+        matches_file = match_features.main(
+            conf=matcher_conf, pairs=loc_pairs, features=feature_conf["output"], export_dir=output_dir
+        )
+        localize_sfm.main(
+            reference_sfm=ref_sfm,
+            queries=query_list,
+            retrieval=loc_pairs,
+            features=feature_file,
+            matches=matches_file,
+            results=results_path,
+        )
 
-# Generate a list of query images with their intrinsics.
-generate_query_lists(timestamps, seq_dir, query_list)
+        # Convert the absolute poses to relative poses with the reference frames.
+        submission_dir.mkdir(exist_ok=True)
+        prepare_submission(results=results_path, relocs=reloc, poses_path=ref_dir / "poses.txt", out_dir=submission_dir)
 
-# Generate the localization pairs from the given reference frames.
-generate_localization_pairs(
-    sequence, reloc, num_loc_pairs, ref_pairs, loc_pairs)
-
-# Extract, match, and localize.
-ffile = extract_features.main(fconf, seq_images, output_dir)
-mfile = match_features.main(mconf, loc_pairs, fconf['output'], output_dir)
-localize_sfm.main(
-    ref_sfm, query_list, loc_pairs, ffile, mfile, results_path)
-
-# Convert the absolute poses to relative poses with the reference frames.
-submission_dir.mkdir(exist_ok=True)
-prepare_submission(results_path, reloc, ref_dir / 'poses.txt', submission_dir)
-
-# If not a test sequence: evaluation the localization accuracy.
-if 'test' not in sequence:
-    logger.info('Evaluating the relocalization submission...')
-    evaluate_submission(submission_dir, reloc)
+        # If not a test sequence: evaluation the localization accuracy.
+        if "test" not in sequence:
+            logger.info("Evaluating the relocalization submission ...")
+            evaluate_submission(submission_dir=submission_dir, relocs=reloc, ths=[0.1, 0.2, 0.5])
--- _my/dissertation/my_hloc/pipelines/4Seasons/prepare_reference.py
+++ _my/dissertation/my_hloc/pipelines/4Seasons/prepare_reference.py
@@ -1,7 +1,7 @@
-from pathlib import Path
 import argparse
-import sys
 import os
+import sys
+from pathlib import Path
 
 from configs import feature_configs, matcher_configs
 
@@ -14,28 +14,31 @@ else:
     print(f"__PYTHONPATH: {user_paths}")
 
 from hloc import extract_features, match_features, pairs_from_poses, triangulation
-from utils2 import get_timestamps, delete_unused_images
-from utils2 import build_empty_colmap_model
+from utils2 import build_empty_colmap_model, delete_unused_images, get_timestamps
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=Path, default='datasets/4Seasons', help='Path to the dataset, default: %(default)s')
-parser.add_argument('--outputs', type=Path, default='outputs/4Seasons', help='Path to the output directory, default: %(default)s')
+parser.add_argument(
+    "--dataset", type=Path, default="datasets/4Seasons", help="Path to the dataset, default: %(default)s"
+)
+parser.add_argument(
+    "--outputs", type=Path, default="outputs/4Seasons", help="Path to the output directory, default: %(default)s"
+)
 parser.add_argument("--num_ref", type=int, default=20, help="Number of image pairs for SfM, default: %(default)s")
 parser.add_argument("--feature_conf", type=str, help="Feature config")
 parser.add_argument("--matcher_conf", type=str, help="Matcher config")
 args = parser.parse_args()
 
-ref_dir = args.dataset / 'reference'
-assert ref_dir.exists(), f'{ref_dir} does not exist'
-ref_images = ref_dir / 'undistorted_images'
+ref_dir = args.dataset / "reference"
+assert ref_dir.exists(), f"{ref_dir} does not exist"
+ref_images = ref_dir / "undistorted_images"
 
 output_dir: Path = args.outputs
 output_dir.mkdir(exist_ok=True, parents=True)
-ref_sfm_empty: Path = output_dir / 'sfm_reference_empty'
-ref_sfm: Path = output_dir / 'sfm_superpoint+superglue'
+ref_sfm_empty: Path = output_dir / "sfm_reference_empty"
+ref_sfm: Path = output_dir / "sfm_superpoint+superglue"
 
 num_ref_pairs: int = args.num_ref
-ref_pairs: Path = output_dir / f'pairs-db-dist{num_ref_pairs}.txt'
+ref_pairs: Path = output_dir / f"pairs-db-dist{num_ref_pairs}.txt"
 
 
 feature_conf: dict = feature_configs[args.feature_conf]
@@ -43,7 +46,7 @@ matcher_conf: dict = matcher_configs[args.matcher_conf]
 
 # Only reference images that have a pose are used in the pipeline.
 # To save time in feature extraction, we delete unsused images.
-delete_unused_images(ref_images, get_timestamps(ref_dir / 'poses.txt', 0))
+delete_unused_images(ref_images, get_timestamps(ref_dir / "poses.txt", 0))
 
 # Build an empty COLMAP model containing only camera and images
 # from the provided poses and intrinsics.
@@ -54,5 +57,5 @@ pairs_from_poses.main(ref_sfm_empty, ref_pairs, num_ref_pairs)
 
 # Extract, match, and triangulate the reference SfM model.
 ffile = extract_features.main(feature_conf, ref_images, output_dir)
-mfile = match_features.main(matcher_conf, ref_pairs, feature_conf['output'], output_dir)
+mfile = match_features.main(matcher_conf, ref_pairs, feature_conf["output"], output_dir)
 triangulation.main(ref_sfm, ref_sfm_empty, ref_images, ref_pairs, ffile, mfile)
