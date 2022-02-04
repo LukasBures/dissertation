1367bf94a434ca6b17ba8f1bf9c6f277b4c9100d
Lukas Bures <burylukas@seznam.cz>
2022-02-04 08:28:39 +0100

    update: segmentation 4seasons exit code

--- _my/dissertation/my_hloc/segmentation/segment_nvidia_4seasons.sh
+++ _my/dissertation/my_hloc/segmentation/segment_nvidia_4seasons.sh
@@ -15,7 +15,7 @@ DESTINATION_FOLDER="/data512/dissertation_results/4Seasons/"
 
 # Segment NVIDIA
 DATASET_NAME="4Seasons_validation_cam0"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 # time python -m runx.runx scripts/4Seasons_dump_validation_cam0.yml -i
@@ -23,7 +23,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 
 # Segment NVIDIA
 DATASET_NAME="4Seasons_validation_cam1"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 # time python -m runx.runx scripts/4Seasons_dump_validation_cam1.yml -i
@@ -32,7 +32,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 # Create h5 file
 DATASET_NAME="4Seasons_validation"
 echo "Starting creating h5 file plus grouping segmentations ..."
-cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit 1
 # python segment.py --dataset_name $DATASET_NAME --path_roots $PATH_ROOT_CAM0 $PATH_ROOT_CAM1 --destination_folder $DESTINATION_FOLDER --segmentation_classes $HUMAN $VEHICLE
 echo "DONE saved to h5 ..."
 echo "-----------------------------------------------"
@@ -48,7 +48,7 @@ DESTINATION_FOLDER="/data512/dissertation_results/4Seasons/"
 
 # Segment NVIDIA
 DATASET_NAME="4Seasons_training_cam0"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 # time python -m runx.runx scripts/4Seasons_dump_training_cam0.yml -i
@@ -56,7 +56,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 
 # Segment NVIDIA
 DATASET_NAME="4Seasons_training_cam1"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 time python -m runx.runx scripts/4Seasons_dump_training_cam1.yml -i
@@ -65,7 +65,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 # Create h5 file
 DATASET_NAME="4Seasons_training"
 echo "Starting creating h5 file plus grouping segmentations ..."
-cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit 1
 python segment.py --dataset_name $DATASET_NAME --path_roots $PATH_ROOT_CAM0 $PATH_ROOT_CAM1 --destination_folder $DESTINATION_FOLDER --segmentation_classes $HUMAN $VEHICLE
 echo "DONE saved to h5 ..."
 echo "-----------------------------------------------"
@@ -81,7 +81,7 @@ DESTINATION_FOLDER="/data512/dissertation_results/4Seasons/"
 
 ## Segment NVIDIA
 DATASET_NAME="4Seasons_test0_cam0"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 time python -m runx.runx scripts/4Seasons_dump_test0_cam0.yml -i
@@ -89,7 +89,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 
 # Segment NVIDIA
 DATASET_NAME="4Seasons_test0_cam1"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 time python -m runx.runx scripts/4Seasons_dump_test0_cam1.yml -i
@@ -98,7 +98,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 # Create h5 file
 DATASET_NAME="4Seasons_test0"
 echo "Starting creating h5 file plus grouping segmentations ..."
-cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit 1
 python segment.py --dataset_name $DATASET_NAME --path_roots $PATH_ROOT_CAM0 $PATH_ROOT_CAM1 --destination_folder $DESTINATION_FOLDER --segmentation_classes $HUMAN $VEHICLE
 echo "DONE saved to h5 ..."
 echo "-----------------------------------------------"
@@ -114,7 +114,7 @@ DESTINATION_FOLDER="/data512/dissertation_results/4Seasons/"
 
 ## Segment NVIDIA
 DATASET_NAME="4Seasons_test1_cam0"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 time python -m runx.runx scripts/4Seasons_dump_test1_cam0.yml -i
@@ -122,7 +122,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 
 # Segment NVIDIA
 DATASET_NAME="4Seasons_test1_cam1"
-cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_segmentation/semantic_segmentation_NVIDIA/ || exit 1
 export OMP_NUM_THREADS=3
 echo "Starting segmentation: $DATASET_NAME ..."
 time python -m runx.runx scripts/4Seasons_dump_test1_cam1.yml -i
@@ -131,7 +131,7 @@ echo "DONE segmentation: $DATASET_NAME ..."
 # Create h5 file
 DATASET_NAME="4Seasons_test1"
 echo "Starting creating h5 file plus grouping segmentations ..."
-cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit
+cd /home/lukas/PycharmProjects/dissertation/_my/dissertation/my_hloc/segmentation/ || exit 1
 python segment.py --dataset_name $DATASET_NAME --path_roots $PATH_ROOT_CAM0 $PATH_ROOT_CAM1 --destination_folder $DESTINATION_FOLDER --segmentation_classes $HUMAN $VEHICLE
 echo "DONE saved to h5 ..."
 echo "-----------------------------------------------"
